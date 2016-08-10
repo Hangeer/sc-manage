76fb88fed9af74549093d19fb132650f4ad5032e
Hangeer <591417089@qq.com>
2016-08-10 15:09:46 +0800

    Add activity CRUD

--- src/components/activity/detail.vue
+++ src/components/activity/detail.vue
@@ -101,9 +101,10 @@
     </select>
 
     <div class="submit-container">
-      <button type="button" class="btn btn-warning" style="float: right" @click="submitActivity">
+      <button type="button" class="btn btn-warning" @click="submitActivity">
         提交
       </button>
+      <p>提示：提交之后点上方 关闭列表 按钮关闭此列表</p>
     </div>
 
   </section>
@@ -115,48 +116,76 @@
       'modifyId'
     ],
     ready () {
-      if (this.modifyId > 0 ) {
-        console.log("传入的 id " + this.modifyId + " 有效情况");
-        let data = {
-          hd_id: this.modifyId
-        };
-        let url = `http://localhost:8360/backend/index/getsingleactivity`;
-        this.$http.post(url, data, {
-          emulateJSON: true
-        })
-        .then((res) => {
-          this.currentActivity = res.data.data;
-        }, (res) => {
-          console.log("获取单条信息失败");
-        });
-      } else {
-        console.log("传入的 id " + this.modifyId + " 无效情况");
-      }
+      this.$options.methods.getActivity.bind(this)();
     },
     data () {
       return {
         currentActivity: {
-
+          hd_title: "",
+          hd_cover: "",
+          hd_place: "",
+          hd_time: "",
+          hd_getTicket_time: "",
+          hd_getTicket_place: "",
+          hd_unit: "",
+          hd_peixun: ""
         }
       }
     },
     methods: {
+      getActivity () {
+        if (this.modifyId > 0 ) {
+          console.log("传入的 id " + this.modifyId + " 有效情况");
+          let data = {
+            hd_id: this.modifyId
+          };
+          let url = `http://localhost:8360/backend/index/getsingleactivity`;
+          this.$http.post(url, data, {
+            emulateJSON: true
+          })
+          .then((res) => {
+            this.currentActivity = res.data.data;
+          }, (res) => {
+            console.log("获取单条信息失败");
+          });
+        } else {
+          console.log("新建活动");
+        }  
+      },
       submitActivity () {
         let hd_detail = document.getElementById("editor-article").innerHTML.trim();
         let hd_compere = document.getElementById("editor-figure").innerHTML.trim();
         let hd_state = document.getElementById("status").value;
         let hd_type = document.getElementById("type").value;
+        let data = this.currentActivity;
+        let url = ``;
 
-        this.currentActivity.hd_detail = hd_detail;
-        this.currentActivity.hd_compere = hd_compere;
-        this.currentActivity.hd_state = hd_state;
-        this.currentActivity.hd_type = hd_type;
+        data.hd_detail = hd_detail;
+        data.hd_compere = hd_compere;
+        data.hd_state = hd_state;
+        data.hd_type = hd_type;
 
         /*
         *   是的没错 这一段写得好智障啊
         *   绑定不了数据
+        *   每次都要判断 this.modifyId 看是不是新建活动
         */
-        
+
+
+        if (this.modifyId > 0) {
+          url = `http://localhost:8360/backend/index/updateactivity`;
+        } else {
+          url = `http://localhost:8360/backend/index/createactivity`;
+        }
+
+        this.$http.post(url, data, {
+            emulateJSON: true
+          })
+          .then((res) => {
+            alert("提交成功");
+          }, (res) => {
+            alert("提交失败");
+          });
       }
     }
   }
--- src/components/activity/list.vue
+++ src/components/activity/list.vue
@@ -38,10 +38,10 @@
             {{item.hd_unit}}
         </td>
         <td>
-          <button type="button" class="btn btn-danger">
+          <button type="button" class="btn btn-danger" @click="deleteSingleActivity(item.id)">
             删除
           </button>
-          <button type="button" class="btn btn-info">
+          <button type="button" class="btn btn-info" @click="modifySingleActivity(item.id)">
             编辑
           </button>
         </td>
@@ -60,20 +60,35 @@
         </ul>
         <div style="width: 300px; margin: 0 auto">
           <span>当前第</span>
-          <input type="text" class="form-control" style="width: 50px; display: inline-block; text-align: center" v-model="currentPage">
+          <input type="text" 
+                 class="form-control" 
+                 style="width: 50px; display: inline-block; text-align: center" 
+                 v-model="currentPage">
           <span>页，一共 {{totalPage}} 页</span>
-          <button class="btn btn-default" type="button" @click="gotoPage">Go!</button>
+          <button class="btn btn-default" 
+                  type="button" 
+                  @click="gotoPage">
+          Go!</button>
         </div>
       </nav>
     </div>
 
     <div style="overflow: hidden">
-      <button type="button" class="btn btn-success right">
+      <button type="button" 
+              class="btn btn-danger" 
+              @click="closeDetail"
+              v-show="isDetailShow">
+        关闭列表
+      </button>
+      <span v-show="isDetailShow">(若未提交不会修改数据)</span>
+      <button type="button" 
+              class="btn btn-success right"
+              @click="createActivity">
         添加新的活动
       </button>
     </div>
     
-    <detail-component v-show="isDetailShow" :modify-id="modifyActivityId"></detail-component>
+    <detail-component v-if="isDetailShow" :modify-id="modifyActivityId"></detail-component>
   </section>
 </template>
 
@@ -89,7 +104,7 @@
     },
     data () {
       return {
-        isDetailShow: true,
+        isDetailShow: false,
         currentPage: 1,
         totalPage: 1,
         activityList: [],
@@ -112,6 +127,40 @@
         }, (res) => {
           console.log(`获取活动列表失败`);
         });
+        /*
+        *  获取列表
+        */
+      },
+      modifySingleActivity (num) {
+        this.modifyActivityId = num;
+        this.isDetailShow == false 
+        ? this.isDetailShow = true 
+        : alert("请先关闭列表 再进行编辑");
+        /*
+        *  编辑单条数据
+        */
+      },
+      deleteSingleActivity (num) {
+        let data = {
+          id: num
+        };
+        let url = `http://localhost:8360/backend/index/deleteactivity`;
+        this.$http.post(url, data, {
+          emulateJSON: true
+        })
+        .then((res) => {
+          alert(`删除成功`);
+          this.$options.methods.getActivityList.bind(this)();
+        }, (res) => {
+          alert(`删除失败`);
+        });
+        /*
+        *  删除单条数据
+        */
+      },
+      createActivity () {
+        this.modifyActivityId = -1;
+        this.isDetailShow = true;
       },
       prevPage () {
         if (this.currentPage == 1) {
@@ -131,6 +180,9 @@
       },
       gotoPage () {      
         this.currentPage >=1 && this.currentPage <= this.totalPage ? this.$options.methods.getActivityList.bind(this)() : alert("无效的页数");
+      },
+      closeDetail () {
+        this.isDetailShow = false;
       }
     }
   }
